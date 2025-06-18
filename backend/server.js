10c78dd4e88d7da7f2b80b667c7d72ee79c6c01b
Sam-Waithaka <sw.waithakasamuel@gmail.com>
2025-06-18 09:29:42 +0300

    nodemon and api routes

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,21 @@ import express from 'express'
 const app = express()
 
 app.get("/api/notes", (req, res)=>{
-    res.status(200).send('you get 5 notes')
+    res.status(200).send('you get 10 notes')
+}) 
+
+app.post('/api/notes', (req, res)=>{
+    res.status(201).json({message:'Post created succesfully'})
 })
+
+app.put('/api/notes/:id', (req, res)=>{
+    res.status(200).json({message:'Post Updated succesfully'})
+})
+
+app.delete('/api/notes/:id', (req, res)=>{
+    res.status(200).json({message: 'Note deleted succsfully'})
+} )
+
 
 app.listen(5001, ()=>{
     console.log('Server started');

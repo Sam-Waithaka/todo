d71cd33358e1d7dbd5123cbcd6f9bd72f1d57041
Sam-Waithaka <sw.waithakasamuel@gmail.com>
2025-06-19 13:07:41 +0300

    feat: integrate Tailwind CSS and DaisyUI for styling
    - Added Tailwind CSS and DaisyUI dependencies in package.json
    - Updated App component to use DaisyUI buttons instead of toast notifications
    - Configured Tailwind CSS in index.css and included DaisyUI plugin
    - Modified vite.config.js to include Tailwind CSS plugin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,19 @@ import {Route, Routes} from "react-router"
 import HomePage from './pages/HomePage'
 import CreatePage from './pages/CreatePage'
 import NoteDetailPage from './pages/NoteDetailPage'
-import toast from 'react-hot-toast'
+// import toast from 'react-hot-toast'
 
 const App = () => {
   return (
     <div>
-      <button onClick={()=> toast.success("success")}>Click</button>
+      <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+        <button className="btn btn-info">Info</button>
+        <button className="btn btn-success">Success</button>
+        <button className="btn btn-warning">Warning</button>
+        <button className="btn btn-error">Error</button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />

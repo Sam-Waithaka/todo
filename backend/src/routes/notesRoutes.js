a554912d91fd114ab24302d710a73b342000d80e
Sam-Waithaka <sw.waithakasamuel@gmail.com>
2025-06-21 16:31:15 +0300

    Adds mern-todo package and updates server configurations
    Integrates the mern-todo package as a local dependency in both frontend and backend.
    
    Enhances server configuration to serve static files in production and enforces CORS policies conditionally based on the environment.
    
    Improves overall application structure and prepares for deployment.

--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -3,10 +3,10 @@ import { createNote, deleteNote, getAllNotes, updateNote, getNotebyId } from '..
 
 const router = express.Router();
 
-export default router;
-
 router.get('/', getAllNotes)
 router.get('/:id', getNotebyId)
 router.post('/', createNote)
 router.put('/:id', updateNote )
 router.delete('/:id', deleteNote)
+
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import cors from 'cors'
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path' 
 
 import rateLimit from './middleware/rateLimiter.js'
 import notesRoutes from './routes/notesRoutes.js'
@@ -11,6 +12,7 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
 
 //Middleware
@@ -23,11 +25,23 @@ app.use(express.json()) // this will parse json bodies: req.body
     
 // })
 
-app.use(cors({
-    origin: "http://localhost:5173",
-}))
+if (process.env.NODE_ENV !== 'production') {
+    app.use(cors({
+        origin: "http://localhost:5173",
+    }))
+}
+
+
 app.use(rateLimit)
 app.use("/api/notes", notesRoutes)
+
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.get(/.*/, (req, res)=>{
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 
 // Connect DB then start app
